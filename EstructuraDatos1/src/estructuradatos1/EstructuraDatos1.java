e5147d9cdefc05f63292775e5aa07258744bb83e
Orlando Montoya <omb0320@gmail.com>
2017-05-31 13:34:15 -0500

    Agrega la estructura Pair

---
+++ EstructuraDatos1/src/estructuradatos1/EstructuraDatos1.java
@@ -0,0 +1,11 @@
+package estructuradatos1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class EstructuraDatos1 {
+    
+}
