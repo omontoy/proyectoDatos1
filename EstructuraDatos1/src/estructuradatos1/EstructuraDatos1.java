9bea4aaf2e0efc50bb0bb53240e562896fdbe360
vcastro04 <vcastro@eafit.edu.co>
2017-05-31 13:42:33 -0500

    Agrega el método buscarSemestre

--- EstructuraDatos1/src/estructuradatos1/EstructuraDatos1.java
+++ EstructuraDatos1/src/estructuradatos1/EstructuraDatos1.java
@@ -7,5 +7,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EstructuraDatos1 {
+    static ArrayList<Pair<String, ArrayList<Pair<String, ArrayList<Pair<String,String>>>>>> semestres = new ArrayList<>();
+    //static ArrayList<Pair<String, ArrayList<Pair<String,String>>>> cursos = new ArrayList<>();
+    //static ArrayList<Pair<String,String>> estudiantes = new ArrayList<>();
+    
+    public static int buscarSemestre(String s){
+        for(int i=0; i<semestres.size(); i++){
+            if(semestres.get(i).getFirst().equals(s)) return i;
+        }
+        return -1;
+    }
     
 }
